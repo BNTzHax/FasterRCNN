4280792bad021742f49c5aa850293ee6be1681eb
BNTzHax <buinhattruong1996@gmail>
2020-11-23 08:08:12 +0000

    Update file

--- frcnn-from-scratch-with-keras/keras_frcnn/simple_parser.py
+++ frcnn-from-scratch-with-keras/keras_frcnn/simple_parser.py
@@ -47,8 +47,7 @@ def get_data(input_path, cat=None):
                     else:
                         all_imgs[filename]['imageset'] = 'test'
 
-            all_imgs[filename]['bboxes'].append(
-                {'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+            all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
 
         all_data = []
         for key in all_imgs:
