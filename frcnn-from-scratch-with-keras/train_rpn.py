cfa155d098a95ebebb2c78f13168993affb36a28
BNTzHax <buinhattruong1996@gmail>
2020-11-23 08:03:54 +0000

    Update file

--- frcnn-from-scratch-with-keras/train_rpn.py
+++ frcnn-from-scratch-with-keras/train_rpn.py
@@ -25,9 +25,9 @@ from keras.utils import generic_utils
 if 'tensorflow' == K.backend():
     import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
-config2 = tf.ConfigProto()
+config2 = tf.compat.v1.ConfigProto()
 config2.gpu_options.allow_growth = True
-set_session(tf.Session(config=config2))
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config2))
 
 # command arg example
 #--network mobilenetv1 -o simple -p ../../datasets/small_kitti_obj/kitti_train.txt
